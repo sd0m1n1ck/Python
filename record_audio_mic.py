e9923ea9cdfed331214e17e23d7687d98afa89e8
Steeve DOMINIQUE <40844173+sd0m1n1ck@users.noreply.github.com>
2018-08-05 00:16:41 -0400

    Update record_audio_mic.py

--- record_audio_mic.py
+++ record_audio_mic.py
@@ -1,3 +1,6 @@
+#The "record-audio-mic.py" is a small program that continuously listens to the PC microphone and starts recording to a WAV file.
+#The recording will stop once there's about 10 seconds silent and will start recording on in a new file once a sound rises.
+
 from array import array
 from struct import pack
 from sys import byteorder
